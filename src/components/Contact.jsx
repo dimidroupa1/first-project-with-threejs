f35325fa643f82328af14a8818c9c1b2dea7fbc6
dimidroupa1 <dimidroupa.react@gmail.com>
2023-08-02 21:37:42 +0300

    finish

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,126 @@
-import React from 'react'
+import { useState, useRef } from 'react'
+import { motion } from 'framer-motion'
+import emailjs from '@emailjs/browser'
+
+import { styles } from '../styles'
+import { EarthCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { slideIn } from '../utils/motion'
 
 const Contact = () => {
+  const formRef = useRef()
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      "service_po6fhtv",
+      "template_ao0zo02",
+      {
+        from_name: form.name,
+        to_name: "DimidrouPa",
+        from_email: form.email,
+        to_email: "dimidroupa.react@gmail.com",
+        message: form.message,
+      },
+      "RDkYQ2NPiiL_B2CkB"
+    ).then(() => {
+      setLoading(false)
+
+      alert('Thank you. I will get back to you as soon as possible.')
+
+      setForm({
+        name: '',
+        email: '',
+        message: ''
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error)
+
+      alert('Something went wrong')
+    })
+  }
+
   return (
-    <div>Contact</div>
-  )
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        className="flex-[0.75] bg-black-100 rounded-2xl p-8"
+        variants={slideIn("left", "tween", 0.2, 1)}
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          action=""
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary text-white rounded-lg py-4 px-6 placeholder:text-secondary outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary text-white rounded-lg py-4 px-6 placeholder:text-secondary outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your message</span>
+            <textarea
+              rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary text-white rounded-lg py-4 px-6 placeholder:text-secondary outline-none border-none font-medium"
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none font-bold shadow-md text-white w-fit rounded-xl shadow-primary"
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto mg:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
 }
 
-export default Contact
+export default SectionWrapper(Contact, 'contact')
